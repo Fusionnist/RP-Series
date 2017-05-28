1b6d1207cb5437424323d2d8b3ead21adb2756ba
Fusionnist <antoine.chartier@gmail.com>
2017-05-28 16:26:53 +0200

    lame

--- Roleplay/TileSheet.cs
+++ Roleplay/TileSheet.cs
@@ -10,27 +10,10 @@ namespace Roleplay
 {
     public class TileSheet
     {
-        public int w, h;
-        public Texture2D src;
         public string[] tiles;
-        public Point[] tpos;
-        public TileSheet(string docpath_, ContentManager c_)
+        public TileSheet(string[] tiles_)
         {
-            XDocument sheetDoc = XDocument.Load(docpath_);
-            List<string> names = new List<string>();
-            List<Point> ps = new List<Point>();
-            w = int.Parse(sheetDoc.Element("Tilesheet").Attribute("w").Value);
-            h = int.Parse(sheetDoc.Element("Tilesheet").Attribute("h").Value);
-
-            src = c_.Load<Texture2D>(sheetDoc.Element("Tilesheet").Attribute("path").Value);
-
-            foreach(XElement el in sheetDoc.Element("Tilesheet").Elements("Tile"))
-            {
-                names.Add(el.Attribute("name").Value);
-                ps.Add(new Point(int.Parse(el.Attribute("x").Value), int.Parse(el.Attribute("y").Value)));
-            }
-            tiles = names.ToArray();
-            tpos = ps.ToArray();
+            tiles = tiles_;
         }
     }
 }
